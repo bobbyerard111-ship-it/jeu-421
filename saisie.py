4661dc9a9e9948a2c25d8cf2665ac86c8b86000a
GabyTheBaby <246833790+BobbyErard-oss@users.noreply.github.com>
2025-12-02 14:08:16 +0100

    Il faut trouver une solution pour cette merde @BobbyErard-oss

--- saisie.py
+++ saisie.py
@@ -6,7 +6,7 @@ def choisir_des_a_relancer():
     while n > 3:
         n = int(input("Veuillez saisir le nombre de dés à relancer (3 au maximum) !"))
     if n == 3:
-        des_à_relancer = ["1", "2", "3"]
+        des_à_relancer = [1, 2, 3]
     elif n == 2:
         des_à_relancer.append(int(input("Quel est le premier dé que vous voulez relancer (indiquez la position du dé à relancer) ?")))
         des_à_relancer.append(int(input("Quel est le second dé que vous voulez relancer (indiquez la position du dé à relancer) ?")))   
@@ -20,8 +20,10 @@ def relancer_main(main, des_à_relancer):
         main = lancer_main()
 
     elif len(des_à_relancer) == 2:
-        main[des_à_relancer[0]] = main(lancer_de)
-        main[des_à_relancer[1]] = main(lancer_de)
+        main[des_à_relancer[0]] = lancer_de()
+        main[des_à_relancer[1]] = lancer_de()
 
     else:
-        main[des_à_relancer[0]] = main(lancer_de)
+        main[des_à_relancer[0]] = lancer_de()
+
+relancer_main([3,2,1], ["1"])
