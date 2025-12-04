677a8ee65ba5419f11176c9e20e605f079a1154c
Joseph <234467960+bobbyerard111-ship-it@users.noreply.github.com>
2025-12-04 22:22:13 +0100

    Derniere update

--- mode_un_joueur.py
+++ mode_un_joueur.py
@@ -38,44 +38,44 @@ def recommend_keep_simple(main_ia):
                     cpt = cpt + 1
                 j = j + 1
             if cpt == 3:
-                kept_indices = [0,1,2]
+                garder_indices = [0,1,2]
                 triple_trouve = True
                 tout_garder = True
             de_i = de_i + 1
 
-    pair_found = False
-    pair_i = -1
-    pair_j = -1
+    paire_trouvee = False
+    paire_i = -1
+    paire_j = -1
     if not tout_garder:
         i = 0
         while i < 3:
             j = i + 1
             while j < 3:
-                if not pair_found:
+                if not paire_trouvee:
                     if main_ia[i] == main_ia[j]:
-                        pair_found = True
-                        pair_i = i
-                        pair_j = j
+                        paire_trouvee = True
+                        paire_i = i
+                        paire_j = j
                 j = j + 1
             i = i + 1
-        if pair_found:
-            kept_indices = [pair_i, pair_j]
+        if paire_trouvee:
+            garder_indices = [paire_i, paire_j]
 
-    if not all_kept and not pair_found:
-        best = -1
-        best_i = -1
+    if not tout_garder and not paire_trouvee:
+        meilleur = -1
+        meilleur_i = -1
         i = 0
         while i < 3:
-            if main_ia[i] > best:
-                best = main_ia[i]
-                best_i = i
+            if main_ia[i] > meilleur:
+                meilleur = main_ia[i]
+                meilleur_i = i
             i = i + 1
-        if best >= 5:
-            kept_indices = [best_i]
+        if meilleur >= 5:
+            garder_indices = [meilleur_i]
         else:
-            kept_indices = []
-    kept_values = []
+            garder_indices = []
+    garder_indices = []
     i = 0
     while i < len(kept_indices):
-        kept_values = kept_values + [main_ia[kept_indices[i]]]
+        garder_indices = garder_indices + [main_ia[kept_indices[i]]]
         i = i + 1
