1342a85fd2936c18ba9962d20b301f1393eb785f
bobbyerard111-ship-it <234467960+bobbyerard111-ship-it@users.noreply.github.com>
2025-12-04 15:56:04 +0100

    IA pour le 421

--- mode_un_joueur.py
+++ mode_un_joueur.py
@@ -1,1 +1,14 @@
+from noyau import *
+from saisie import *
+from affichage import *
 
+def choix_ia(main_ia):
+    indices = []
+    priorite = [4, 2, 1]
+
+    for valeur in priorite:
+        for i in range(len(main_ia)):
+            if des[i] == valeur:
+                indices.append(i)
+
+    return indices
