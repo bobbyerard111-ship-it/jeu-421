1e5fe36c615a2519c8cee47e773163a01c657bef
Joseph <234467960+bobbyerard111-ship-it@users.noreply.github.com>
2025-12-05 22:58:36 +0100

    Réorganisation des fichiers et des fonctions dans les fichiers.

--- 421.py
+++ 421.py
@@ -1,7 +1,13 @@
 from noyau import *
 from affichage import *
+from saisie import *
 
 def distribution_des_jetons(nb_jetons_total=11, afficher=True):
+    """
+    Entrée: nombre total de jetons à distribuer (11) et un booléen pour savoir s'il faut afficher les résultats
+    Sortie: nombre de jetons gagnés par le joueur 1
+    But: distribuer aléatoirement les jetons entre deux joueurs
+    """
     jetons_joueur1 = 0
     jetons_joueur2 = 0
     jetons_restants = nb_jetons_total
@@ -60,3 +66,27 @@ def bataille(jetons_joueur1, jetons_joueur2):
                grand_vainqueur = 2
         
     return grand_vainqueur                     #pt il vaut mieux que ce soit un return 1 ou 2 dans la boucle plutot que de mettre dans une variable que l'on return dans ce cas la pas besoin de boucle while ou a modifier et aussi faudrait voir si ca correspond au return demandé
+
+def jouer_tour():
+    """
+    Entrée: aucune
+    Sortie: la main finale du joueur après le tour
+    But: effectuer le tour complet d’un joueur (lancer la main initiale, permettre au joueur de relancer certains dés jusqu’à 2 fois, afficher les mains avant et après relance, retourner la main finale)
+    """
+    main = trier_main(lancer_main())
+    afficher_main(main)
+
+    relances_restantes = 2  
+    relancer = True        
+
+    while relances_restantes > 0 and relancer:
+        des_a_relancer = choisir_des_a_relancer()  # demander quels dés relancer
+        if des_a_relancer == []:                   # si le joueur ne relance rien
+            relancer = False
+        else:
+            main = relancer_main(main, des_a_relancer)  
+            main = trier_main(main)
+            afficher_main(main)
+            relances_restantes -= 1
+
+    return main
