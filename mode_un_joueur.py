c81da8d7b7e18aaaaae889a42780d090fd4e6679
Joseph <234467960+bobbyerard111-ship-it@users.noreply.github.com>
2025-12-07 15:32:05 +0100

    Modification des choix de l'IA

--- mode_un_joueur.py
+++ mode_un_joueur.py
@@ -3,81 +3,69 @@ from saisie import *
 from affichage import *
 
 def choix_ia(main_ia):
+    """
+    Décide quels dés l'IA doit relancer pour obtenir la meilleure main.
+    Retourne une liste d'indices (0, 1, 2) des dés à relancer.
+    """
+    main_triee = trier_main(main_ia)
 
-    garder_indices = []
-    c1 = 0
-    c2 = 0
-    c4 = 0
+    if est_421(main_triee):
+        return []  # Ne rien relancer - 421 est la meilleure main!
+    
+    if est_brelan(main_triee):
+        return []  # Ne rien relancer - Brelan est déjà très bon
+    
+    if est_suite(main_triee):
+        return []  # Ne rien relancer - Suite (1-2-3, 2-3-4, etc)
+    
+    if est_fiche(main_triee):
+        return []  # Fiche (deux dés identiques) = assez bon, on garde
+    
+    # ÉTAPE 2: Logique pour améliorer la main
+    
+    # Une paire est une bonne base pour tenter le brelan
     i = 0
-
     while i < 3:
-        if main_ia[i] == 1:
-            c1 = c1 + 1
-        if main_ia[i] == 2:
-            c2 = c2 + 1
-        if main_ia[i] == 4:
-            c4 = c4 + 1
+        j = i + 1
+        while j < 3:
+            if main_ia[i] == main_ia[j]:
+                # On a trouvé une paire! On relance le dé qui n'est pas dans la paire !
+                indices_a_relancer = []
+                for k in range(3):
+                    if k != i and k != j:
+                        indices_a_relancer.append(k)
+                return indices_a_relancer
+            j = j + 1
         i = i + 1
-    if c1 > 0 and c2 > 0 and c4 > 0:
-        garder_indices = [0,1,2]
-
-    tout_garder = False
-    if len(garder_indices) == 3:
-        tout_garder = True
-
-    if not tout_garder:
-        liste_de = [1,2,3,4,5,6]
-        de_i = 0
-        triple_trouve = False
-        while de_i < 6:
-            de = liste_de[de_i]
-            cpt = 0
-            j = 0
-            while j < 3:
-                if main_ia[j] == de:
-                    cpt = cpt + 1
-                j = j + 1
-            if cpt == 3:
-                garder_indices = [0,1,2]
-                triple_trouve = True
-                tout_garder = True
-            de_i = de_i + 1
-
-    paire_trouvee = False
-    paire_i = -1
-    paire_j = -1
-    if not tout_garder:
-        i = 0
-        while i < 3:
-            j = i + 1
-            while j < 3:
-                if not paire_trouvee:
-                    if main_ia[i] == main_ia[j]:
-                        paire_trouvee = True
-                        paire_i = i
-                        paire_j = j
-                j = j + 1
-            i = i + 1
-        if paire_trouvee:
-            garder_indices = [paire_i, paire_j]
-
-    if not tout_garder and not paire_trouvee:
-        meilleur = -1
-        meilleur_i = -1
-        i = 0
-        while i < 3:
-            if main_ia[i] > meilleur:
-                meilleur = main_ia[i]
-                meilleur_i = i
-            i = i + 1
-        if meilleur >= 5:
-            garder_indices = [meilleur_i]
-        else:
-            garder_indices = []
-    garder_indices = []
+    
+    #  ÉTAPE 3: Si pas de paire, stratégie d'amélioration intelligente
+    
+    # Trouver le plus grand dé pour évaluer la main
+    meilleur = -1
+    meilleur_i = -1
+    deuxieme = -1
+    deuxieme_i = -1
+    
     i = 0
-    while i < len(garder_indices):
-        garder_indices = garder_indices + [main_ia[garder_indices[i]]]
+    while i < 3:
+        if main_ia[i] > meilleur:
+            deuxieme = meilleur
+            deuxieme_i = meilleur_i
+            meilleur = main_ia[i]
+            meilleur_i = i
+        elif main_ia[i] > deuxieme:
+            deuxieme = main_ia[i]
+            deuxieme_i = i
         i = i + 1
+    
+    # Stratégie 2: Si on a au moins un dé >= 4, le garder + un autre dé et relancer le 3e
+    if meilleur >= 4:
+        indices_a_relancer = []
+        for k in range(3):
+            if k != meilleur_i and k != deuxieme_i:
+                indices_a_relancer.append(k)
+        return indices_a_relancer
+    
+    # Stratégie 3: Si tous les dés sont petits (< 4), on relance tout pour avoir une chance
 
-    return garder_indices
+    return [0, 1, 2]
